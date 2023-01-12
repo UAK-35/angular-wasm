a59f0612bc9be25227c0a0b74323faa704855145
Umar Ali Khan <Umar.Ali@BeyondSolutions.co>
2023-01-12 15:52:05 +0500

    removed docker commands and tested wasm build with local emscripten SDK

--- src/app/wasm/fibonacci/build-cmd.js
+++ src/app/wasm/fibonacci/build-cmd.js
@@ -1,2 +1,2 @@
 exports.cmd =
-  "docker run --rm -v $(pwd):/src emscripten/emsdk emcc -Os src/app/wasm/fibonacci/fibonacci.c -o src/assets/wasm/fibonacci.wasm --no-entry";
+  "emcc -Os src/app/wasm/fibonacci/fibonacci.c -o src/assets/wasm/fibonacci.wasm --no-entry";
